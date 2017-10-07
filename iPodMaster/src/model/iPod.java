59fc722cb0ddd1cadac604958d783a9113c9dd73
jyoo980 <jyoo980@gmail.com>
2017-10-06 21:14:28 -0700

    Resolved iPod-master issues

--- iPodMaster/src/model/iPod.java
+++ iPodMaster/src/model/iPod.java
@@ -10,7 +10,7 @@ import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Set;
 
-public class qiPod implements Iterable<AbstractMedia> {
+public class iPod implements Iterable<AbstractMedia> {
 
     private String name;
     private Set<AbstractMedia> movies = new HashSet<>();
